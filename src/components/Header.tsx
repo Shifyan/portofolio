117e366a6270f60b4d75331ee70afc895cae657a
Shifyannn <asifyan01@gmail.com>
2025-12-19 16:05:41 +0700

    add ActionButton and Badge components; update Header and HomePage to utilize them

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,44 +1,34 @@
 import Link from "next/link";
-import { Button } from "./ui/button";
 import { PAGE_ROUTES } from "@/lib/routes";
-import Image from "next/image";
+import { ActionButton } from "./ActionButton";
 
 export default function Header() {
   const classForCenterHeader =
-    "flex justify-between items-center gap-[46px] py-[18px] px-[44px] rounded-[90px] bg-white/60 border border-white/80 shadow-[0_0_1px_rgba(0,0,0,0.3),_0_2px_30px_rgba(0,0,0,0.08),_0_0_15px_rgba(0,0,0,0.03)] backdrop-blur-[7.5px] text-[14px] font-semibold text-[#475467]";
+    "flex items-center gap-[46px] py-[18px] px-[44px] rounded-[90px] bg-white/60 border border-white/80 shadow-[0_0_1px_rgba(0,0,0,0.3),_0_2px_30px_rgba(0,0,0,0.08),_0_0_15px_rgba(0,0,0,0.03)] backdrop-blur-[7.5px] text-[14px] font-semibold text-[#475467] whitespace-nowrap w-max";
+
   return (
-    <div className="flex justify-between items-center py-[30px] px-[42px]">
-      <Link href={PAGE_ROUTES.HOME} className="text-2xl font-bold">
-        Shif &bull; <span className="text-[#667085]">Yannn</span>
-      </Link>
-      <div className={classForCenterHeader}>
-        <Link href={PAGE_ROUTES.HOME}>Home</Link>
-        <Link href={PAGE_ROUTES.PORTOFOLIO}>Portofolio</Link>
-        <Link href={PAGE_ROUTES.ABOUT}>About Me</Link>
-        <Link href={PAGE_ROUTES.CONTACT}>Contact</Link>
+    <header className="grid grid-cols-3 items-center py-[30px] px-[42px] w-full">
+      <div className="flex justify-start">
+        <Link
+          href={PAGE_ROUTES.HOME}
+          className="text-2xl font-bold whitespace-nowrap"
+        >
+          Shif &bull; <span className="text-[#667085]">Yannn</span>
+        </Link>
       </div>
 
-      <div>
-        <Button variant="outline" asChild className="py-6 px-7 rounded-[78px]">
-          <Link
-            href={PAGE_ROUTES.LINKEDIN}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex justify-center items-center"
-          >
-            <Image
-              src="/Social-icon.svg"
-              alt="LinkedIn"
-              width={20}
-              height={20}
-              className="inline"
-            />
-            <span className="text-lg text-[#344054] font-semibold">
-              Get In Touch
-            </span>
-          </Link>
-        </Button>
+      <div className="flex justify-center">
+        <div className={classForCenterHeader}>
+          <Link href={PAGE_ROUTES.HOME}>Home</Link>
+          <Link href={PAGE_ROUTES.PORTOFOLIO}>Portofolio</Link>
+          <Link href={PAGE_ROUTES.ABOUT}>About Me</Link>
+          <Link href={PAGE_ROUTES.CONTACT}>Contact</Link>
+        </div>
       </div>
-    </div>
+
+      <div className="flex justify-end">
+        <ActionButton />
+      </div>
+    </header>
   );
 }
--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,3 +1,34 @@
+import { Badge } from "@/components/ui/badge";
+import { ActionButton } from "@/components/ActionButton";
 export default function HomePage() {
-  return <div>Welcome to the Home Page</div>;
+  return (
+    <div className="py-10">
+      <div className="flex justify-center gap-2 text-[#475467] mb-4">
+        <Badge variant="outline" className="text-[#475467]">
+          React Developer
+        </Badge>
+        <Badge variant="outline" className="text-[#475467]">
+          Front-End Developer
+        </Badge>
+        <Badge variant="outline" className="text-[#475467]">
+          Back-End Developer
+        </Badge>
+      </div>
+      <div className="mb-7">
+        <div>
+          <h1 className="font-bold text-[59px] text-center mb-3">
+            <span className="text-[#667085]">Hello!</span> I am Shifyan
+            Almustafid, a Web Developer Based in Indonesia
+          </h1>
+          <h3 className="flex justify-center text-[#667085]">
+            Passionate about building end-to-end web applications and
+            transforming ideas into real-world solutions.
+          </h3>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <ActionButton text="Contact Me" />
+      </div>
+    </div>
+  );
 }
